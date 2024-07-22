8e9cfb0c6b36033afea64f6a8666444f95194071
Bartosz Kotwica <bartekbkotwica@gmail.com>
2024-07-22 11:38:56 +0200

    working the writing_textures

--- main.c
+++ main.c
@@ -12,13 +12,41 @@ int	main(void)
 	main_str.hero->ma = malloc(sizeof(t_map));
 	initilize_map(main_str.hero->ma);
 	read_map(main_str.hero->ma);
-	checker(main_str.hero, main_str.hero->ma);
+	// checker(main_str.hero, main_str.hero->ma);
 	ext_map(main_str.hero);
 	printf("%s", main_str.hero->ma->no);
-	return (0);
 	main_str.con = mlx_init();
 	main_str.win = mlx_new_window(main_str.con, 1000, 1000, "tmp");
 	write_textures(&main_str);
+	int	y = -1;
+	int	x = -1;
+	// while (++ y < 64)
+	// {
+		// x = -1;
+		// while (++ x < 64)
+			// printf("y = %d, x = %d, var = %d\n", y, x, main_str.tex.w[y][x]);
+	// }
+	main_str.img = mlx_new_image(main_str.con, 1000, 1000);
+	main_str.addr = mlx_get_data_addr(main_str.img, &main_str.bits_per_pixel, &main_str.line_length, &main_str.endian);
+	y = -1;
+	int	i = -1;
+	int	j = -1;
+	while (++ i < 64)
+	{
+		j = -1;
+		x = -1;
+		y = i % 64;
+		while (++ j < 64)
+		{
+			my_mlx_pixel_put(&main_str, j, i, main_str.tex.s[y][x]);
+			x ++;
+		}
+		y ++;
+	}
+	mlx_put_image_to_window(main_str.con, main_str.win, main_str.img, 0, 0);
+	mlx_loop(main_str.con);
+	
+	return (0);
 	// main_str.img = mlx_new_image(main_str.con, 1000, 1000);
 	// void	*texture;
 	// int		bits_per_pixel;
@@ -47,8 +75,8 @@ int	main(void)
 	// int	gre = ((*(unsigned int *)(main_str.addr + pos)) >> 8) & 0xFF;
 
 
-	int	y = -1;
-	int	x = -1;
+	y = -1;
+	x = -1;
 	// while (++ y < img_height)
 	// {
 	// 	x = -1;
@@ -69,8 +97,8 @@ int	main(void)
 	main_str.img = mlx_new_image(main_str.con, 1000, 1000);
 	main_str.addr = mlx_get_data_addr(main_str.img, &main_str.bits_per_pixel, &main_str.line_length, &main_str.endian);
 	y = -1;
-	int i = -1;
-	int j = -1;
+	i = -1;
+	j = -1;
 	while (++ i < 64)
 	{
 		j = -1;
